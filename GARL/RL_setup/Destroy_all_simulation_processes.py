cc3b203b9b64c198a2edb354a07a60c0605fd12e
Noimps <nemoterbogt2002@icloud.com>
2025-06-26 14:17:45 +0200

    Cleanup and clearance

--- GARL/RL_setup/Destroy_all_simulation_processes.py
+++ GARL/RL_setup/Destroy_all_simulation_processes.py
@@ -5,6 +5,14 @@ import signal
 import subprocess
 
 
+"""
+When anything goes south during developing, run this script to make sure all hidden/side/server processes get closed
+
+If not done, and too many processes are running, you ram is going to get full and things will go south even more (linux gonna restart or shutdown)
+
+
+
+"""
 def close_sim():
 
 
